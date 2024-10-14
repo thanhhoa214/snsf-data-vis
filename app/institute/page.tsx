f5e737b0eadbfea295225e4abccd0d03b1121463
Hoa Nguyen <hoanguyen.rin@gmail.com>
2024-10-14 16:53:07 +1100

    a

--- app/institute/page.tsx
+++ app/institute/page.tsx
@@ -1,31 +1,28 @@
-import { links } from "@/components/ui2/Navbar";
+import Navbar from "@/components/ui2/Navbar";
 import { prisma } from "@/lib/prisma/client";
-import { cn } from "@/lib/utils";
-import Link from "next/link";
 
 export default async function Home() {
   const grantCount = await prisma.grant.count();
   const ins = prisma.institute.count();
   return (
-    <main className="min-h-dvh flex flex-col items-center gap-4 justify-center">
-      <h1>Choose your dashboard</h1>
-      {grantCount} {ins}
-      <ul className="flex justify-center items-center gap-8">
-        {links.map(({ label, icon, className }) => (
-          <li key={label}>
-            <Link
-              href={`/${label.toLowerCase()}`}
-              className={cn(
-                "flex flex-col justify-center items-center p-4 w-40 aspect-video border rounded-xl",
-                className
-              )}
-            >
-              {icon}
-              <strong>{label}</strong>
-            </Link>
-          </li>
-        ))}
-      </ul>
+    <main className="space-y-4">
+      <Navbar />
+      <h1 className="mb-4 text-center">Institute Dashboard</h1>
+      <p>
+        Insti No: {grantCount} {ins}
+      </p>
+      {/* <PersonFilter
+        serverItems={await searchInstitutes()}
+        initItem={institute}
+        itemKey="InstituteNumber"
+        itemLabel="Institute"
+        onSearch={searchInstitutes}
+      /> */}
+      {/* <Top5DisciplinesHighestAwards institute={institute.Institute} />
+      <FundTrendForDiscipline
+        disciplines={uniqueDisciplines}
+        chartData={chartData}
+      /> */}
     </main>
   );
 }
