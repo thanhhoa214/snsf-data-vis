d906b41e5285403d4d341b2a59064bc6f380059f
Hoa Nguyen <hoanguyen.rin@gmail.com>
2024-10-14 17:52:41 +1100

    t

--- app/institute/page.tsx
+++ app/institute/page.tsx
@@ -2,7 +2,11 @@ import Navbar from "@/components/ui2/Navbar";
 import { getInstituteByNumber } from "../actions/institute";
 import Top5DisciplinesHighestAwards from "./Top5DisciplinesHighestAwards";
 
-export default async function Home() {
+export default async function Home({
+  searchParams,
+}: {
+  searchParams?: { [key: string]: string | string[] | undefined };
+}) {
   const institute = await getInstituteByNumber(0);
 
   if (!institute) {
@@ -13,6 +17,7 @@ export default async function Home() {
     <main className="space-y-4">
       <Navbar />
       <h1 className="mb-4 text-center">Institute Dashboard</h1>
+      search params: {JSON.stringify(searchParams)}
       {/* <PersonFilter
         serverItems={await searchInstitutes()}
         initItem={institute}
