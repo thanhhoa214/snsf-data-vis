e118e61511ea1b9c8c21ccc7155d8c4bf776618f
Hoa Nguyen <hoanguyen.rin@gmail.com>
2024-10-14 17:55:38 +1100

    institue

--- app/institute/page.tsx
+++ app/institute/page.tsx
@@ -1,35 +1,52 @@
+import FundTrendForDiscipline from "@/components/ui2/FundTrendForDiscipline";
 import Navbar from "@/components/ui2/Navbar";
-import { getInstituteByNumber } from "../actions/institute";
+import PersonFilter from "@/components/ui2/PersonFilter";
+import { getDisciplineLineData } from "../actions/discipline";
+import { getInstituteByNumber, searchInstitutes } from "../actions/institute";
 import Top5DisciplinesHighestAwards from "./Top5DisciplinesHighestAwards";
 
-export default async function Home({
-  searchParams,
-}: {
-  searchParams?: { [key: string]: string | string[] | undefined };
-}) {
+export default async function Home() {
   const institute = await getInstituteByNumber(0);
 
   if (!institute) {
     return <main>No institute found</main>;
   }
 
+  const uniqueDisciplines: Array<{
+    MainDiscipline: string;
+    MainDisciplineNumber: number;
+  }> = [];
+  institute.grants.forEach((grant) => {
+    if (
+      !uniqueDisciplines.some(
+        (discipline) =>
+          discipline.MainDisciplineNumber === grant.MainDisciplineNumber
+      )
+    ) {
+      uniqueDisciplines.push({
+        MainDiscipline: grant.MainDiscipline,
+        MainDisciplineNumber: grant.MainDisciplineNumber,
+      });
+    }
+  });
+  const chartData = await getDisciplineLineData(uniqueDisciplines);
+
   return (
     <main className="space-y-4">
       <Navbar />
       <h1 className="mb-4 text-center">Institute Dashboard</h1>
-      search params: {JSON.stringify(searchParams)}
-      {/* <PersonFilter
+      <PersonFilter
         serverItems={await searchInstitutes()}
         initItem={institute}
         itemKey="InstituteNumber"
         itemLabel="Institute"
         onSearch={searchInstitutes}
-      /> */}
+      />
       <Top5DisciplinesHighestAwards institute={institute.Institute} />
-      {/* <FundTrendForDiscipline
+      <FundTrendForDiscipline
         disciplines={uniqueDisciplines}
         chartData={chartData}
-      /> */}
+      />
     </main>
   );
 }
