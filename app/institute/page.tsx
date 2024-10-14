2fc9549bc1f5c834477cdd66b49f814bafe11499
Hoa Nguyen <hoanguyen.rin@gmail.com>
2024-10-15 10:53:19 +1100

    push institue

--- app/institute/page.tsx
+++ app/institute/page.tsx
@@ -1,5 +1,7 @@
 import Navbar from "@/components/ui2/Navbar";
 import PersonFilter from "@/components/ui2/PersonFilter";
+import { Loader } from "lucide-react";
+import { Suspense } from "react";
 import { searchInstitutes } from "../actions/institute";
 import Container from "./Container";
 
@@ -15,7 +17,16 @@ export default async function Home() {
         onSearch={searchInstitutes}
       />
 
-      <Container />
+      <Suspense
+        fallback={
+          <div className="h-80 w-full flex flex-col justify-center items-center">
+            <Loader className="animate-spin" />
+            Loading...
+          </div>
+        }
+      >
+        <Container />
+      </Suspense>
     </main>
   );
 }
